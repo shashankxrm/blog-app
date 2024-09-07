eca4a37dac87060df510f5dc18b1cb0d5e91d340
Shashank RM <ugs22047_ece.shashank@cbit.org.in>
2024-09-08 00:23:28 +0530

    fixed errors

--- app/admin/addBlog/page.jsx
+++ app/admin/addBlog/page.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 import React, { useState } from 'react'
 import { toast } from 'react-toastify'
 
-const page = () => {
+const Page = () => {
 
     const [image,setImage] = useState(false);
     const [data,setData] = useState({
